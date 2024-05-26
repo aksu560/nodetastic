0dba6b6adafea702fcc0b4696f65fd4637c17f5b
Aksu <aksu@venho.net>
2024-05-26 14:39:12 +0300

    Added additional tests

--- src/test.rs
+++ src/test.rs
@@ -120,3 +120,43 @@ pub fn test_directed_edge_removal() {
 
     assert_eq!(graph, target_graph);
 }
+
+#[test]
+pub fn test_get_node() {
+    let mut graph = Graph::new();
+    graph.add_node();
+    graph.add_node();
+    graph.add_node();
+    assert_eq!(graph.get_node(0).unwrap().id, 0);
+    assert_eq!(graph.get_node(1).unwrap().id, 1);
+    assert_eq!(graph.get_node(2).unwrap().id, 2);
+    assert_eq!(graph.get_node(3), None);
+}
+
+#[test]
+pub fn test_get_edge() {
+    let mut graph = Graph::new();
+    graph.add_node();
+    graph.add_node();
+    graph.add_node();
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 0);
+    assert_eq!(graph.get_edge(0).unwrap().id, 0);
+    assert_eq!(graph.get_edge(1).unwrap().id, 1);
+    assert_eq!(graph.get_edge(2).unwrap().id, 2);
+    assert_eq!(graph.get_edge(3), None);
+}
+
+#[test]
+pub fn test_get_edge_between() {
+    let mut graph = Graph::new();
+    graph.add_node();
+    graph.add_node();
+    graph.add_node();
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    assert_eq!(graph.get_edge_between(0, 1).unwrap().id, 0);
+    assert_eq!(graph.get_edge_between(1, 2).unwrap().id, 1);
+    assert_eq!(graph.get_edge_between(2, 0), None);
+}
